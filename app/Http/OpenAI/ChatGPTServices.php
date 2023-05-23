879abd3cf16b1ea806126f60c0fd84a9386f974d
mansuri <mansuri.dev0@gmail.com>
2023-05-23 14:59:06 +0330

    Reformat and cleaning ChatGPTServices class

--- app/Http/OpenAI/ChatGPTServices.php
+++ app/Http/OpenAI/ChatGPTServices.php
@@ -5,32 +5,22 @@ namespace App\Http\OpenAI;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+
 class ChatGPTServices
 {
-
-    public function sendChatGPTRequest(array $questions)
+    /**
+     * @param array $questions
+     * @return array [$ChatGPTAPIResponse, $responseCode]
+     */
+    public function sendChatGPTRequest(array $questions): array
     {
-
-
-        if(count($questions) == 1){
-            return $this->sendChatGPTResponseRequest($questions);
-        }
-
-        $data = [];
-        foreach ($questions as $key => $question){
-            $data[] = $question[0];
-            if ($key == count($questions)-1)
-                break;
-            $data[] = $question[1];
-        }
 
         $res = Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization' => 'Bearer '.env('OPEN_AI_SECRET_KEY'),
-        ])
-            ->post("https://api.openai.com/v1/chat/completions", [
+        ])->post("https://api.openai.com/v1/chat/completions", [
                 "model" => "gpt-3.5-turbo",
-                'messages' => $data,
+                'messages' => $this->toAcceptableFormat($questions),
                 'temperature' => 0.8,
                 "max_tokens" => 3500,
                 "top_p" => 0.9,
@@ -39,47 +29,30 @@ class ChatGPTServices
                 "stop" => ["11."],
             ]);
 
+
         if($res->status() == 200){
             return [$res->json()["choices"][0]["message"], 200];
         }
 
-        return [$res->json()["error"], 429];
-
+        return [$res->json()["error"], $res->status()];
 
     }
 
-
-    public function sendChatGPTResponseRequest(array $questions)
+    /**
+     * @param array $questions
+     * @return array
+     */
+    private function toAcceptableFormat(array $questions): array
     {
-
-
-
-        $res = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer '.env('OPEN_AI_SECRET_KEY'),
-        ])
-            ->post("https://api.openai.com/v1/chat/completions", [
-                "model" => "gpt-3.5-turbo",
-                'messages' => [
-                    [
-                        "role" => "user",
-                        "content" => $questions[0][0]["content"]
-                    ]
-                ],
-                'temperature' => 0.5,
-                "max_tokens" => 4000,
-                "top_p" => 0.8,
-                "frequency_penalty" => 0.52,
-                "presence_penalty" => 0.5,
-                "stop" => ["11."],
-            ]);
-
-        if($res->status() == 200){
-            return [$res->json()["choices"][0]["message"], 200];
+        $data = [];
+        foreach ($questions as $key => $question) {
+            $data[] = $question[0];
+            if ($key == count($questions) - 1)
+                break;
+            $data[] = $question[1];
         }
-
-        return [$res->json()["error"], 429];
-
+        return $data;
     }
+
 
 }
